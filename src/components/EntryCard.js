d61eaff287bfe825ad648c7a6136e52564cde6c5
Enzine <sonja.makipaja@kolumbus.fi>
2017-08-29 23:37:39 +0300

    add showable things to EntryCard

--- src/components/EntryCard.js
+++ src/components/EntryCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
+import '../css/EntryCard.css';
 
 export default class EntryCard extends Component {
   render() {
@@ -11,10 +12,22 @@ export default class EntryCard extends Component {
           </div>
           <div className="card-header">
             <div className="card-title h5">{moment(this.props.entry.createdAt).format('dd DD.MM')}</div>
-            <div className="card-subtitle text-gray"></div>
+            <div className="card-subtitle text-gray">
+              <span className="label label-secondary label-rounded">
+                Activity {this.props.entry.generalActivity}
+              </span>
+            </div>
           </div>
           <div className="card-body">
-            {this.props.entry.generalComments}
+            <label className="form-switch">
+              <input disabled checked={this.props.entry.generalPoop} type="checkbox" />
+              <i className="form-icon"></i> pooped
+            </label>
+            <label className="form-switch">
+              <input disabled checked={this.props.entry.generalBath} type="checkbox" />
+              <i className="form-icon"></i> bathed
+            </label>
+            <p className="entry-card-text">{this.props.entry.generalComments}</p>
           </div>
           <div className="card-footer">
             <Link to={"/entries/" + this.props.entry.id} className="btn">View</Link>
